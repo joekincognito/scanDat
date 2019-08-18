993510929b2266aa11f08f77127a79168438fd33
joekincognito <joekincognito@gmail.com>
2019-08-18 13:43:42 -0500

    consignment serial number scannin test 1

---
+++ js/consignment.js
@@ -0,0 +1,18 @@
+$('#scan').click(function(){
+    //var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+    cordova.plugins.barcodeScanner.scan( function (result) {         
+        // if(!(result.text.toString().length===5 || result.text.toString().length===6)){
+        //     alert("Scan Error or invalid barcode\n" +
+        //      "Please Try Again!");
+        // }
+        // else 
+        // {
+            //ajax(result.text,null);
+            //$('#item').val( result.text);
+            $('#info').append("<p>Scan Result " + result.text + "</p>"); 
+        }
+    }, function (error) { 
+        $('#info').append("<p>Scanning failed: " + error + "</p>"); 
+    });
+    
+});
