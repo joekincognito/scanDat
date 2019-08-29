44f689a2953ae8ba8a3de91474c9029cf54d5e65
joekincognito <joekincognito@gmail.com>
2019-08-28 19:33:52 -0500

    Front to Back Connection Take 6 aka damn semi colon aaaaand Action

--- js/consignment.js
+++ js/consignment.js
@@ -67,7 +67,7 @@ function hasSerialNumberMatch(serial_number) {
             crossDomain: true,
             //username: 'ScanAppFloorPlanAccount',
             //password: 'WordPassIsNotaGoodPassword!',
-            data: "customer_number=" + user.customer_id; + "&serial_number=" + serial_number,
+            data: "customer_number=" + user.customer_id + "&serial_number=" + serial_number,
             //data: "customer_number=2501&serial_number=12345",
             statusCode: {
                 404: function() {
